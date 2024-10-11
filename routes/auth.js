881dedcd26138ebdec262ab194d610b640e66a8b
ClericCoder <noibisjunior22@gmail.com>
2024-10-11 23:41:12 +0100

    added the user balance routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const router = express.Router();
-
+const {verifyToken} = require('../middlewares/authMiddleware.js');
 // Correct way to import functions
-const { login, register, forgotPassword,resetPassword,logOut } = require('../controller/auth.js');
+const { login, 
+        register, 
+        forgotPassword,
+        resetPassword,
+        logOut } = require('../controller/auth.js');
+
+const {getUserBalances} = require('../controller/userController.js')
 
 // Creating POST routes
 router.route('/api/auth/register').post(register);
 router.route('/api/auth/login').post(login);
 router.route('/api/auth/forgot-password').post(forgotPassword);
 router.route('/api/auth/reset-password/:tokens').post(resetPassword);
-
 router.route('/api/auth/logOut').post(logOut)
+
+router.route('/api/users/balances').get(verifyToken,getUserBalances);
+
+
 module.exports = router;
 
 
